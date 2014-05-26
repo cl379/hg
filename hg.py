bc87f9d5cd9acc690db5656e9298b9a2b17f7b1a
Carla Lancelotti <carla.lancelotti@upf.edu>
2014-05-26 18:17:51 +0200

    multiple agents + mating

--- hg.py
+++ hg.py
@@ -21,10 +21,14 @@ class HG():
         self._world = world
         self._home = Coordinate (-1,-1)
         self._homeRange = 10
+        self._population = []
+        self._population.append(15)
+        self._population.append(15)
 
     def step(self, step):
         print('HG executing step: ', step)
         self.settle()
+        self.trackDemography()
         self.doForage()
 
     def settle(self):
@@ -32,11 +36,33 @@ class HG():
         self._home = self._world.getRandomDune()
         print('I am here: ', self._home)
 
+    def trackDemography(self):
+        for i in range(len(self._population)):
+            if i>1 and self._population[i]>=15:
+                self.createAgent(i)
+            self._population[i] = self._population[i]+1
+        if random.randint(0, 1) == 1:
+            self._population.append(0)
+        print('\tDemography: ', self._population)
+
+    def createAgent(self, index):
+        for agent in self._world._agents:
+            mateIndex = agent.getAdultChild()
+            if mateIndex !=-1:
+                newAgent = AP()
+            myWorld.addAgent(myAP)
+        return
+
+    def getAdultChild(self):
+        for i in range(len(self._population)):
+            if i>1 and self._population[i]>=15:
+                return i
+        return -1
+
+
     def doForage(self):
         print('\tResources collected')
 
-    def trackDemography(self):
-        print('\tDemography')
 
 
 class World():
@@ -47,6 +73,7 @@ class World():
             for j in range(size):
                 cellType = random.randint(cellTypes.eDune, cellTypes.eInterdune)
                 self._ground[i][j] = cellType
+        self._agents = []
 
 
     def getRandomDune(self):
@@ -58,11 +85,22 @@ class World():
         index = random.randint(0, len(candidates))
         return candidates[index]
 
+    def checkPosition(self, location):
+        if location._x<0 or location._y<0:
+            return False
+        if location._x>self._size or location._y>self._size:
+            return False
+        return True
+
+    def addAgent(self, agent):
+        self._agents.append(agent)
+        agent._world  = self
+
 def main():
     size = 100
     myWorld = World(size)
     myHG = HG(myWorld)
-    timeSteps = 10
+    timeSteps = 50
     for i in range(timeSteps):
         print('Executig time step: ', i)
         myHG.step(i)

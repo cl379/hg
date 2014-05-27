a7e9d0fc32f2b0b2977b49472d8166f1d2f6dd17
Carla Lancelotti <carla.lancelotti@upf.edu>
2014-05-27 18:54:11 +0200

    mateIndex, refined checkLocation, started doForage

--- hg.py
+++ hg.py
@@ -7,6 +7,7 @@ def enum(**enums):
     return type('Enum', (), enums)
 
 cellTypes = enum(eDune=0, eWater=1, eInterdune=2)
+cellStates = enum(eVisited=0, eEmpty=1, eFull=2)
 
 
 class Coordinate():
@@ -14,27 +15,29 @@ class Coordinate():
         self._x = x
         self._y = y
     def __str__(self):
-        return str(self._x)+','+str(sel._y)
+        return str(self._x)+','+str(self._y)
 
 class HG():
     def __init__(self, world):
         self._world = world
         self._home = Coordinate (-1,-1)
-        self._homeRange = 10
+        self._homeRange = 50
         self._population = []
         self._population.append(15)
         self._population.append(15)
 
+    def __str__(self):
+        return str(self._x)+','+str(self._y)
+
     def step(self, step):
-        print('HG executing step: ', step)
+        print 'HG executing step: ', step
         self.settle()
         self.trackDemography()
         self.doForage()
 
     def settle(self):
-        print('\tSettle')
         self._home = self._world.getRandomDune()
-        print('I am here: ', self._home)
+        print 'I am here: ', self._home
 
     def trackDemography(self):
         for i in range(len(self._population)):
@@ -43,15 +46,24 @@ class HG():
             self._population[i] = self._population[i]+1
         if random.randint(0, 1) == 1:
             self._population.append(0)
-        print('\tDemography: ', self._population)
+        print '\tDemography: ', self._population
 
     def createAgent(self, index):
         for agent in self._world._agents:
             mateIndex = agent.getAdultChild()
-            if mateIndex !=-1:
-                newAgent = AP()
-            myWorld.addAgent(myAP)
-        return
+            if mateIndex != -1:
+                newAgent = HG()
+                self._world.addAgent(newAgent)
+                if random.randint(0,1) ==0:
+                    newAgent._home = self._home
+                else:
+                    newAgent._home - agent._home
+                newAgent._population[0] = self._population[index]
+                newAgent._population[1] = agent._population[mateIndex]
+                self._population[index] = -1
+                self._population[mateIndex] = -1
+                return True
+        return False
 
     def getAdultChild(self):
         for i in range(len(self._population)):
@@ -61,7 +73,24 @@ class HG():
 
 
     def doForage(self):
-        print('\tResources collected')
+        for i in range(self._homeRange,1+self._homeRange):
+            for j in range(self._homeRange,1+self._homeRange):
+                location = Coordinate(i+self._home._x,j+self._home._y)
+                if not self._world.checkPosition(location):
+                    continue
+                if self._world._ground[location._x][location._y] == cellTypes.eInterdune:
+                    if self._world._groundState[location._x][location._y] != cellStates.eFull:
+                        self.searchFood
+        self.collectBiomass
+
+    def searchFood(self):
+        return
+
+
+    def collectBiomass(self):
+        food = self._world._calories
+        print('\tResources collected: ', food)
+        return
 
 
 
@@ -69,12 +98,17 @@ class World():
     def __init__(self, size):
         self._size = size
         self._ground = [[0 for x in range(size)] for x in range(size)]
+        self._groundState = [[0 for x in range(size)] for x in range(size)]
+        self._calories = [[0 for x in range(size)] for x in range(size)]
         for i in range(size):
             for j in range(size):
                 cellType = random.randint(cellTypes.eDune, cellTypes.eInterdune)
                 self._ground[i][j] = cellType
         self._agents = []
 
+    def addAgent(self, agent):
+        self._agents.append(agent)
+        agent._world  = self
 
     def getRandomDune(self):
         candidates = []
@@ -92,10 +126,7 @@ class World():
             return False
         return True
 
-    def addAgent(self, agent):
-        self._agents.append(agent)
-        agent._world  = self
-
+    
 def main():
     size = 100
     myWorld = World(size)
